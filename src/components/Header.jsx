b678079ede413ef8985627e190b602a44ba03b03
Raul Correa <raulcorrea0110@hotmail.com>
2022-10-23 20:29:12 -0500

    se cambio de nombre correo}

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
             <div className="navbar-right">
                 <ul>
                     <li className="navbar-email" onClick={handleToggle}>
-                        raulcorrea0110@hotmail.com
+                        FabiannaDuffo@gmail.com
                     </li>
                     <li className="navbar-shopping-cart" onClick={() => setToggleOrders(!toggleOrders)}>
                         <img src={shoppingCart} alt="shopping cart" />
